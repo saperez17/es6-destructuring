58b6fedeb0a8dfedeee15f2c20afaac65d5a3d68
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-05-11 22:22:24 -0500

    Proper object/array destructuring

--- src/index.js
+++ src/index.js
@@ -5,18 +5,24 @@ import cars from "./practice";
 
 const [honda, tesla] = cars;
 const {
-  speedStats: { topSpeed: teslaTopSpeed },
-  coloursByPopularity: teslaTopColour
+  speedStats: { topSpeed: teslaTopSpeed }
 } = tesla;
 const {
-  speedStats: { topSpeed: hondaTopSpeed },
-  coloursByPopularity: hondaTopColour
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour]
 } = tesla;
 ReactDOM.render(
   <table>
     <tr>
       <th>Brand</th>
       <th>Top Speed</th>
+      <th>Favourite Colour</th>
     </tr>
     <tr>
       <td>{tesla.model}</td>
